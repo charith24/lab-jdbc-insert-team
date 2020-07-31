92abcf8bd86f669d9f931fe90ddb9cebe25b6d9e
charith24 <charith.charri@gmail.com>
2020-07-31 16:06:32 +0530

    1576033

--- src/dao/CityDAO.java
+++ src/dao/CityDAO.java
@@ -1,1 +1,37 @@
+package dao;
 
+
+import utility.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.*;
+public class CityDAO{
+	public City getCityByName(String name) throws IOException, SQLException {
+		Connection con = ConnectionManager.getConnection();
+		String sql="Select * from city";
+		PreparedStatement st=con.prepareStatement(sql);
+		ResultSet rs=st.executeQuery(sql);
+		while(rs.next()) {
+			if(name.equals(rs.getString(2))) {
+				System.out.println("ID is : " + rs.getInt(1) + "\t City name is : "+ rs.getString(2) );
+			}
+		}
+		return null;
+	}
+	public void createCity(City city) throws SQLException, IOException {
+		long id=city.getCityId();
+		String cityname=city.getCityName();
+		Connection cm = ConnectionManager.getConnection();
+		String sql="INSERT INTO CITY(id,city_name)VALUES(?,?)";
+		PreparedStatement st=cm.prepareStatement(sql);
+		st.setLong(1,id);
+		st.setString(2, cityname);
+		st.executeUpdate();
+		cm.close();	
+	}
+}
